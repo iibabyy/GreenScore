c9a3db262e77bf413acdffe3a997bc7724b03d35
JudaNana <moussaadamou898@gmail.com>
2025-02-15 23:54:49 +0100

    end of the day doctolib

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -16,6 +16,25 @@ document.getElementById("modal").addEventListener("click", function (e) {
 	}
 });
 
+function showResult() {
+	const modalBody = document.getElementById("test");
+	const nbResult = 100;
+	// TODO: appeler le backend de Idrissa 
+	// on recupere le resultat
+	modalBody.innerHTML = ""
+	
+	const nbResultText = document.createElement('p');
+	nbResultText.innerHTML = `${nbResult} Results`;
+	modalBody.appendChild(nbResultText);
+	
+	for (let i = 0; i < nbResult; i++) {
+		const oneMedicine = document.createElement('div');
+		oneMedicine.classList.add("border-2", "border-solid", "border-black", "rounded-sm", "h-24");
+		oneMedicine.innerHTML = "ahaha";
+		modalBody.appendChild(oneMedicine);
+	}
+
+}
 
 function openModal(message) {
 	const modal = document.getElementById("modal");
@@ -27,18 +46,20 @@ function openModal(message) {
 		modal.classList.add("flex");
 		modalContent.classList.remove("scale-95", "opacity-0");
 		modalContent.classList.add("scale-100", "opacity-100");
-		userPrompt.innerText = message;
 	}, 10);
+	userPrompt.innerText = message;
+	showResult();
 }
 
 function sendPrompt() {
 	let prompt = document.getElementById("search");
 
-	if (prompt === null)
+	if (prompt === null || prompt.value === "")
 		return;
 
+	
 	openModal(prompt.value);
-	console.log(prompt.value);
+	console.log(`[${prompt.value}]`);
 	prompt.value = "";
 }
 
