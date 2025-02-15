7ab46c1cf27a836d3c7946f8f17a103fe0c96ae7
idrissa <utileidrissa@gmail.com>
2025-02-15 23:43:22 +0100

    system_prompt made | bug: json responses from ia model are invalid (single quotes instead of double quotes)

---
+++ backend/models.py
@@ -0,0 +1,35 @@
+import json
+from dtos import ProductDto
+from llm import get_green_score
+from pydantic import BaseModel
+
+class DescriptionField(BaseModel):
+	score: int
+	details: str
+
+class ProductDescription(BaseModel):
+	persistence: DescriptionField
+	bioaccumulation: DescriptionField
+	toxicity: DescriptionField
+	ecotoxicological_effects: DescriptionField
+	degradation_byproducts: DescriptionField
+	release_into_environment: DescriptionField
+	regulatory_status: DescriptionField
+	mitigation_strategies: list[str]
+
+
+
+class Product:
+	def __init__(self, infos: ProductDto):
+		self.id = id
+		self.name = infos.name.lower()
+		self.infos = infos;
+
+		# Get the green score and description
+		green_result = get_green_score(self.infos)
+		self.note: int = int(green_result["score"])
+		print(green_result["description"])
+		self.description: ProductDescription = json.loads(str(green_result["description"]))
+
+	def __repr__(self):
+		return f"Product(id={self.id}, name={self.name}, greenScore={self.greenScore}, pertinence={self.pertinence})"
