7ac8124ee3a8e7ea7d85de2fdf0dc230d80c9175
iibabyy <utileidrissa@gmail.com>
2025-02-16 02:58:06 +0100

    save

--- backend/dtos.py
+++ backend/dtos.py
@@ -1,34 +1,43 @@
 
+import json
 from pydantic import BaseModel
 
 
 class DegradabilityDto(BaseModel):
-		water: str
-		soil: str
-		degradation_duration: int	# in month
+	water: str
+	soil: str
+	degradation_duration: int	# in month
 
 class ToxicityDto(BaseModel):
-		aquatic: str
-		terrestrial: str
-		wildlife_impact: str | None
+	aquatic: str
+	terrestrial: str
+	wildlife_impact: str | None
 
 class ProductionsDto(BaseModel):
-		energy_consumed: int	# in kWh/kg
-		co2_emissions: float		# in kg CO2/kg
-		chemical_waste: int		# in kg/kg
+	energy_consumed: int	# in kWh/kg
+	co2_emissions: float		# in kg CO2/kg
+	chemical_waste: int		# in kg/kg
 
 class DisposalDto(BaseModel):
-		recyclable: bool
-		hazardous_waste: bool
-		recommendations: str | None = None
+	recyclable: bool
+	hazardous_waste: bool
+	recommendations: str | None = None
 
 class ProductDto(BaseModel):
-		name: str
-		type: str
-		active_ingredient: str
-		concentration: int		# in mg
-		administration_route: str
-		degradability: DegradabilityDto
-		toxicity: ToxicityDto
-		production: ProductionsDto
-		disposal: DisposalDto
+	name: str
+	type: str
+	active_ingredient: str
+	concentration: int		# in mg
+	administration_route: str
+	degradability: DegradabilityDto
+	toxicity: ToxicityDto
+	production: ProductionsDto
+	disposal: DisposalDto
+
+	def as_json(self) -> str:
+		return json.dumps(
+			self,
+			default=lambda o: o.__dict__,
+			sort_keys=True,
+			indent=4
+		)
