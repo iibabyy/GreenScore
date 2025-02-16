8beb033c974d00581a2058612a9eaf8e626c3bfa
Mohamed Dembele <mdembele@bess-f2r1s19.clusters.42paris.fr>
2025-02-16 04:24:58 +0100

    add abiut

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -10,6 +10,7 @@ function closeModalFunc() {
 	}, 300);
 }
 
+
 document.getElementById("modal").addEventListener("click", function (e) {
 	if (e.target === modal) {
 		closeModalFunc();
@@ -51,6 +52,47 @@ function openModal(message) {
 	showResult();
 }
 
+
+async function InfoModal(message) {
+	document.getElementById("test").innerHTML = "";
+	const about = document.getElementById("test");
+	//add div form About.html to the about modal
+	
+	const modal = document.getElementById("modal");
+	const modalContent = modal.querySelector("div");
+	const userPrompt = document.getElementById("user-prompt");
+	
+	modal.classList.remove("hidden");
+	setTimeout(() => {
+		modal.classList.add("flex");
+		modalContent.classList.remove("scale-100", "opacity-0");
+		modalContent.classList.add("scale-100", "opacity-100");
+	}, 10);
+	about.innerHTML = await fetch("scripts/donuts/donuts.html").then((response) => response.text());
+	userPrompt.innerText = message;
+}
+
+
+
+async function AboutModal(message) {
+	document.getElementById("test").innerHTML = "";
+	const about = document.getElementById("test");
+	//add div form About.html to the about modal
+	
+	const modal = document.getElementById("modal");
+	const modalContent = modal.querySelector("div");
+	const userPrompt = document.getElementById("user-prompt");
+	
+	modal.classList.remove("hidden");
+	setTimeout(() => {
+		modal.classList.add("flex");
+		modalContent.classList.remove("scale-100", "opacity-0");
+		modalContent.classList.add("scale-100", "opacity-100");
+	}, 10);
+	about.innerHTML = await fetch("../About.html").then((response) => response.text());
+	userPrompt.innerText = message;
+}
+
 function sendPrompt() {
 	let prompt = document.getElementById("search");
 
@@ -68,6 +110,16 @@ document.getElementById("search").addEventListener('keypress', (event) => {
 		sendPrompt();
 });
 
+document.getElementById("About").addEventListener('click', async() => {
+	AboutModal("About page");
+});
 
+document.getElementById("learn-more").addEventListener('click', async() => {
+	AboutModal("Learn more");
+});
+
+document.getElementById("How-it-works").addEventListener('click', async() => {
+	InfoModal("Learn more");
+});
 window.sendPrompt = sendPrompt;
 window.closeModalFunc = closeModalFunc;
